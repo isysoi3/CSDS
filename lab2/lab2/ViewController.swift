0952104b4ac870c8cb1894bb82dcfedf4a824766
Ilya Sysoi <14ilya12322@gmail.com>
2019-10-02 19:04:05 +0300

    project init

---
+++ lab2/lab2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lab2
+//
+//  Created by Ilya Sysoi on 10/2/19.
+//  Copyright © 2019 Ilya Sysoi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
